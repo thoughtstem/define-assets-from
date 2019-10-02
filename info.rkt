931ab57fc4a685c0118f69fc2ad39e20910643a9
srfoster <stephen@thoughtstem.com>
2019-10-02 12:21:40 -0700

    extra deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "define-assets-from")
-(define deps '("base"))
+(define deps '("base" "htdp-lib" "scribble-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/define-assets-from.scrbl" ())))
 (define pkg-desc "Description Here")
